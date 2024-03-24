3adef7735986bb46737d166e2e13fcc130d85c35
admin.dev@ebridge-world.com <thd6517@naver.com>
2024-03-24 17:49:10 +0900

    fix

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
     <div class="welcome-message" v-if="isLoggedIn">
-      <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
-      <p class="description">오늘도 즐거운 식물 관리하세요.</p>
+      <p class="welcome-text">
+        안녕하세요, <span class="user-name">{{ user.name }}</span
+        >님!
+      </p>
+      <p class="description">오늘zz도 즐거운 식물 관리하세요.</p>
     </div>
-    <div class="brand" v-if="isLoggedIn">
-      LEAFY
-    </div>
-    <NavBar v-if="isLoggedIn"></NavBar>    
+    <div class="brand" v-if="isLoggedIn">LEAFY</div>
+    <NavBar v-if="isLoggedIn"></NavBar>
     <div class="router-view-wrapper">
       <router-view></router-view>
     </div>
-    <footer class="footer" v-if="isLoggedIn"> 
+    <footer class="footer" v-if="isLoggedIn">
       <p>&copy; 2023 Leafy. All rights reserved.</p>
     </footer>
-    <BasicPopup :message="popup.message" :status="popup.status" :visible="popup.visible" @close="closePopup" />
+    <BasicPopup
+      :message="popup.message"
+      :status="popup.status"
+      :visible="popup.visible"
+      @close="closePopup"
+    />
   </div>
 </template>
 
 <script>
 import BasicPopup from "@/components/BasicPopup.vue";
-import NavBar from '@/components/NavBar.vue';
+import NavBar from "@/components/NavBar.vue";
 import { mapState } from "vuex";
 
 export default {
@@ -33,15 +39,15 @@ export default {
     isLoggedIn() {
       return !!this.$store.state.user;
     },
-    ...mapState(["popup", "user"])
+    ...mapState(["popup", "user"]),
   },
   data() {
     return {
       showModal: false,
       modalContent: {
-        title: '',
-        body: '',
-        footer: '',
+        title: "",
+        body: "",
+        footer: "",
       },
     };
   },
@@ -51,7 +57,7 @@ export default {
     };
   },
   methods: {
-    closePopup() {      
+    closePopup() {
       this.$store.commit("setPopup", {
         ...this.popup,
         visible: false,
@@ -73,12 +79,12 @@ body {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  position: relative; 
+  position: relative;
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -93,7 +99,7 @@ body {
 .brand {
   font-size: 2.5em;
   font-weight: bold;
-  color: #8FBC8F;
+  color: #8fbc8f;
   padding-top: 30px;
   padding-bottom: 20px;
 }
@@ -123,7 +129,7 @@ body {
 }
 
 .user-name {
-  color: #8FBC8F;
+  color: #8fbc8f;
 }
 
 .description {
@@ -132,7 +138,7 @@ body {
 }
 
 .footer {
-  background-color: #8FBC8F;
+  background-color: #8fbc8f;
   color: white;
   padding: 1rem;
   font-size: 0.9rem;
